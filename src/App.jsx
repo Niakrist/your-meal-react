7ccccdb4c23feae7d06989b43e10adee2349199b
Niakrist <ak_88dron@inbox.ru>
2024-08-05 16:30:56 +0300

    feat: emplement navigation.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Container from "./components/container/Container";
 import Header from "./components/header/Header";
+import Navigation from "./components/navigation/Navigation";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Navigation />
+      </main>
     </>
   );
 }
