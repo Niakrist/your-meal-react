341c4815485108e0d5140b80f4eb42847304326d
Niakrist <ak_88dron@inbox.ru>
2024-08-06 08:48:40 +0300

    feat: emlement footer

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -8,37 +8,35 @@ import Product from "../product/Product";
 const Catalog = () => {
   return (
     <section className={s.catalog}>
-      <Container>
-        <div className={s.catalog__container}>
-          <div className={cn(s.catalog__order, s.order)}>
-            <Order />
-          </div>
+      <Container className={s.catalog__container}>
+        <div className={cn(s.catalog__order, s.order)}>
+          <Order />
+        </div>
 
-          <div className={s.catalog__wrapper}>
-            <h2 className={s.catalog__title}>Бургеры</h2>
+        <div className={s.catalog__wrapper}>
+          <h2 className={s.catalog__title}>Бургеры</h2>
 
-            <div className={s.catalog__wrap_list}>
-              <ul className={s.catalog__list}>
-                <li className={s.catalog__item}>
-                  <Product />
-                </li>
-                <li className={s.catalog__item}>
-                  <Product />
-                </li>
-                <li className={s.catalog__item}>
-                  <Product />
-                </li>
-                <li className={s.catalog__item}>
-                  <Product />
-                </li>
-                <li className={s.catalog__item}>
-                  <Product />
-                </li>
-                <li className={s.catalog__item}>
-                  <Product />
-                </li>
-              </ul>
-            </div>
+          <div className={s.catalog__wrap_list}>
+            <ul className={s.catalog__list}>
+              <li className={s.catalog__item}>
+                <Product />
+              </li>
+              <li className={s.catalog__item}>
+                <Product />
+              </li>
+              <li className={s.catalog__item}>
+                <Product />
+              </li>
+              <li className={s.catalog__item}>
+                <Product />
+              </li>
+              <li className={s.catalog__item}>
+                <Product />
+              </li>
+              <li className={s.catalog__item}>
+                <Product />
+              </li>
+            </ul>
           </div>
         </div>
       </Container>
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -6,47 +6,38 @@ import cn from "classnames";
 const Navigation = () => {
   return (
     <nav className={s.navigation}>
-      <Container>
-        <div className={s.container}>
-          <ul className={s.list}>
-            <li className={s.item}>
-              <button
-                className={cn(s.button, s.button_burger, s.button_active)}>
-                Бургеры
-              </button>
-            </li>
-            <li className={s.item}>
-              <button className={cn(s.button, s.button_snack)}>Закуски</button>
-            </li>
-            <li className={s.item}>
-              <button className={cn(s.button, s.button_hotdog)}>
-                Хот-доги
-              </button>
-            </li>
-            <li className={s.item}>
-              <button className={cn(s.button, s.button_combo)}>Комбо</button>
-            </li>
-            <li className={s.item}>
-              <button className={cn(s.button, s.button_shawarma)}>
-                Шаурма
-              </button>
-            </li>
-            <li className={s.item}>
-              <button className={cn(s.button, s.button_pizza)}>Пицца</button>
-            </li>
-            <li className={s.item}>
-              <button className={cn(s.button, s.button_wok)}>Вок</button>
-            </li>
-            <li className={s.item}>
-              <button className={cn(s.button, s.button_dessert)}>
-                Десерты
-              </button>
-            </li>
-            <li className={s.item}>
-              <button className={cn(s.button, s.button_sauce)}>Соусы</button>
-            </li>
-          </ul>
-        </div>
+      <Container className={s.container}>
+        <ul className={s.list}>
+          <li className={s.item}>
+            <button className={cn(s.button, s.button_burger, s.button_active)}>
+              Бургеры
+            </button>
+          </li>
+          <li className={s.item}>
+            <button className={cn(s.button, s.button_snack)}>Закуски</button>
+          </li>
+          <li className={s.item}>
+            <button className={cn(s.button, s.button_hotdog)}>Хот-доги</button>
+          </li>
+          <li className={s.item}>
+            <button className={cn(s.button, s.button_combo)}>Комбо</button>
+          </li>
+          <li className={s.item}>
+            <button className={cn(s.button, s.button_shawarma)}>Шаурма</button>
+          </li>
+          <li className={s.item}>
+            <button className={cn(s.button, s.button_pizza)}>Пицца</button>
+          </li>
+          <li className={s.item}>
+            <button className={cn(s.button, s.button_wok)}>Вок</button>
+          </li>
+          <li className={s.item}>
+            <button className={cn(s.button, s.button_dessert)}>Десерты</button>
+          </li>
+          <li className={s.item}>
+            <button className={cn(s.button, s.button_sauce)}>Соусы</button>
+          </li>
+        </ul>
       </Container>
     </nav>
   );
